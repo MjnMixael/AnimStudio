773751277f02c87573f0251b3b7550303645a54e
Mike Nelson <mjn.other@gmail.com>
2025-06-26 16:25:40 -0500

    better names

--- AnimStudio/Animation/BuiltInPalettes.cpp
+++ AnimStudio/Animation/BuiltInPalettes.cpp
@@ -1,12 +1,12 @@
 #include "BuiltInPalettes.h"
 
 static const QVector<BuiltInPalette> builtInPalettes = {
-    { "Shield",     QVector<QRgb>(std::begin(ShieldPalette), std::end(ShieldPalette)) },
-    { "HUD",        QVector<QRgb>(std::begin(HudPalette), std::end(HudPalette)) },
-    { "FS1 Ship",   QVector<QRgb>(std::begin(Fs1SelectShipPalette), std::end(Fs1SelectShipPalette)) },
-    { "FS1 Weapon", QVector<QRgb>(std::begin(Fs1SelectWepPalette), std::end(Fs1SelectWepPalette)) },
-    { "FS2 Ship",   QVector<QRgb>(std::begin(Fs2SelectShipPalette), std::end(Fs2SelectShipPalette)) },
-    { "FS2 Weapon", QVector<QRgb>(std::begin(Fs2SelectWepPalette), std::end(Fs2SelectWepPalette)) },
+    { "Shield",          QVector<QRgb>(std::begin(ShieldPalette), std::end(ShieldPalette)) },
+    { "HUD",             QVector<QRgb>(std::begin(HudPalette), std::end(HudPalette)) },
+    { "FS1 Ship Icon",   QVector<QRgb>(std::begin(Fs1SelectShipPalette), std::end(Fs1SelectShipPalette)) },
+    { "FS1 Weapon Icon", QVector<QRgb>(std::begin(Fs1SelectWepPalette), std::end(Fs1SelectWepPalette)) },
+    { "FS2 Ship Icon",   QVector<QRgb>(std::begin(Fs2SelectShipPalette), std::end(Fs2SelectShipPalette)) },
+    { "FS2 Weapo Iconn", QVector<QRgb>(std::begin(Fs2SelectWepPalette), std::end(Fs2SelectWepPalette)) },
 };
 
 const QVector<BuiltInPalette>& getBuiltInPalettes() {
